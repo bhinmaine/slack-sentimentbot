87cd2f88b286f3a7bf8379eb5fcbccd2d1b6a428
Benjamin Hamilton <ben@hmltn.org>
2018-12-16 16:46:19 -0500

    challenges accepted

--- infobot/event.py
+++ infobot/event.py
@@ -1,10 +1,16 @@
 import json
 
+
 def receive(event, context):
     data = json.loads(event['body'])
     print("Got data: {}".format(data))
+    return_body = "ok"
+
+    if data["type"] == "url_verification":
+        print("Received challenge")
+        return_body = data["challenge"]
 
     return {
         "statusCode": 200,
-        "body": "ok"
+        "body": return_body
     }
