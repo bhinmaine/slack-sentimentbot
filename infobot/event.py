2f846f62cab36f4a101e557830c5809c7ca05439
Benjamin Hamilton <ben@hmltn.org>
2018-12-16 18:17:59 -0500

    a very simple response

--- infobot/event.py
+++ infobot/event.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 from slackclient import SlackClient
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
@@ -13,8 +14,29 @@ def receive(event, context):
     if data["type"] == "url_verification":
         print("Received challenge")
         return_body = data["challenge"]
+    elif (
+        data["type"] == "event_callback" and
+        data["event"]["type"] == "message" and
+        "subtype" not in data["event"]
+    ):
+        handle_message(data)
 
     return {
         "statusCode": 200,
         "body": return_body
     }
+
+def handle_message(data):
+    poster_user_id = data["event"]["user"]
+
+    p = re.compile(r"ura bot")
+    m = p.findall(data["event"]["text"])
+    if m:
+        for match in m:
+            print(match)
+            print(poster_user_id)
+            sc.api_call(
+                "chat.postMessage",
+                channel="{}".format(data["event"]["channel"]),
+                text="no, ura bot <@{}>".format(poster_user_id)
+                )
