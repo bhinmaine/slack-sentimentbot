afe893b7701a74247646a2459d03277e0109f40f
Benjamin Hamilton <ben@hmltn.org>
2018-12-18 18:20:06 -0500

    as it turns out I'm not good at natural language processing.

--- infobot/event.py
+++ infobot/event.py
@@ -45,22 +45,3 @@ def handle_message(data):
                 channel="{}".format(data["event"]["channel"]),
                 text="no, ura {0}".format(split[1])
                 )
-    
-    # lets discover some factoids
-    if (
-        the_text.find("is") > 0 and
-        the_text[-1] is not "?"
-    ):
-        the_text_as_list = the_text.split()
-        the_subject = the_text_as_list[the_text_as_list.index("is")-1]
-        the_fact = the_text[the_text.find("is")+3:]
-        put_factoid(the_subject, the_fact)
-
-def put_factoid(subject, fact):
-    print("the factoid: {0} is {1}".format(subject, fact))
-    item = {
-        "subject": subject.lower(),
-        "fact": fact.lower()
-    }
-    response = factoid_table.put_item(Item=item)
-    print(response)
