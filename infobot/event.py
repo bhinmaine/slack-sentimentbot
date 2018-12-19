a3ad08d81be4b21e115462c5d3b723ee7d6a0d17
Benjamin Hamilton <ben@hmltn.org>
2018-12-18 21:54:55 -0500

    quick and dirty feature flagging

--- infobot/event.py
+++ infobot/event.py
@@ -7,11 +7,13 @@ from slackclient import SlackClient
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
 SENTIMENT_TABLE = os.environ['SENTIMENT_TABLE']
+FEATUREFLAG_TABLE = os.environ['FEATUREFLAG_TABLE']
 
 sc = SlackClient(BOT_TOKEN)
 ddb = boto3.resource('dynamodb')
 comprehend = boto3.client('comprehend')
 sentiment_table = ddb.Table(SENTIMENT_TABLE)
+featureflag_table = ddb.Table(FEATUREFLAG_TABLE)
 
 def receive(event, context):
     data = json.loads(event['body'])
@@ -34,11 +36,15 @@ def receive(event, context):
     }
 
 def handle_message(data):
-    sentiment = get_sentiment(data["event"]["text"])
-    reaction = get_reaction(sentiment)
-    reaction_response = send_reaction(data["event"]["channel"], reaction, data["event"]["ts"])
-    print(reaction_response)
-    
+    feature_enabled = check_feature_flag("sentiment_reactions")
+    if feature_enabled:
+        sentiment = get_sentiment(data["event"]["text"])
+        reaction = get_reaction(sentiment)
+        reaction_response = send_reaction(data["event"]["channel"], reaction, data["event"]["ts"])
+        print(reaction_response)
+    else:
+        print("sentiment_reactions was disabled")
+
 def get_sentiment(text):
     sentiment = comprehend.detect_sentiment(
         Text=text,
@@ -65,3 +71,11 @@ def send_reaction(channel, reaction, timestamp):
         timestamp=timestamp
     )
     return(response['ok'])
+
+def check_feature_flag(feature):
+    response = featureflag_table.get_item(
+        Key={
+            'feature': feature
+        }
+    )
+    return(response['Item']['enabled'])
