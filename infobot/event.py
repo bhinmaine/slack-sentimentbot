b0597f19effb5803ae97071016ad89ca9505a6e7
Benjamin Hamilton <ben@hmltn.org>
2018-12-18 20:31:39 -0500

    ditched factoids and found AWS Comprehend
    We're going to start examining things said in slack through AWS comprehend.

--- infobot/event.py
+++ infobot/event.py
@@ -6,10 +6,12 @@ from boto3.dynamodb.conditions import Key
 from slackclient import SlackClient
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
-FACTOID_TABLE = os.environ['FACTOID_TABLE']
+SENTIMENT_TABLE = os.environ['SENTIMENT_TABLE']
+
 sc = SlackClient(BOT_TOKEN)
 ddb = boto3.resource('dynamodb')
-factoid_table = ddb.Table(FACTOID_TABLE)
+comprehend = boto3.client('comprehend')
+sentiment_table = ddb.Table(SENTIMENT_TABLE)
 
 def receive(event, context):
     data = json.loads(event['body'])
@@ -34,14 +36,8 @@ def receive(event, context):
 def handle_message(data):
     the_text = data["event"]["text"]
 
-    # some silliness
-    p = re.compile(r"ura \w*")
-    m = p.findall(the_text)
-    if m:
-        for match in m:
-            split = match.split()
-            sc.api_call(
-                "chat.postMessage",
-                channel="{}".format(data["event"]["channel"]),
-                text="no, ura {0}".format(split[1])
-                )
+    sentiment = comprehend.detect_sentiment(
+        Text=the_text,
+        LanguageCode='en'
+    )
+    print(sentiment["Sentiment"])
